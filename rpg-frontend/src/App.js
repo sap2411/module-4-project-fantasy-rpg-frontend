19ca9f951e5ab429d391d9c51038062b94381695
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-08 14:39:10 -0700

    Conditionally render navbar based on login

--- rpg-frontend/src/App.js
+++ rpg-frontend/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import Game from './components/Game.js';
 // import About from './components/About.js';
 import CreateAccount from './components/CreateAccount.js';
 import LogIn from './components/LogIn.js'
-// import CharacterSelect from './components/game_route/CharacterSelectPage.js';
 
 
 
@@ -18,16 +17,28 @@ function App() {
   const gameSavesURL = backendURL + '/game_saves'
   const modifiersURL = backendURL + '/modifiers'
 
+  // Fake login state via state hook
+  const [user, setUser] = useState()
+
+  // Login user
+  const logIn = (user) => {
+    setUser(user)
+  }
+
+  // Logout user
+  const logOut = () => {
+    setUser()
+  }
 
   return (
     <Router>
       <div>
-        <Navbar />
+        <Navbar user={user} logOut={logOut}/>
         <Route exact path="/new-game" component={Game}/>
         {/* <Route exact path="/load-game" component={load game ...} /> */}
         {/* <Route exact path="/about" component={About} /> */}
 
-        <Route exact path="/create-account" component={() => <CreateAccount usersURL={usersURL}/>} />
+        <Route exact path="/create-account" component={() => <CreateAccount usersURL={usersURL} logIn={logIn}/>} />
         <Route exact path="/Log-in" component={LogIn} />
       </div>
     </Router>
--- rpg-frontend/src/components/Navbar.js
+++ rpg-frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {NavLink} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({user, logOut}) => {
     return (
         <header className="navbar navbar-expand navbar-dark bg-primary shadow flex-column flex-md-row bd-navbar">
             <nav className="collapse navbar-collapse" >
@@ -16,38 +16,52 @@ const Navbar = () => {
                             <span className="d-none d-sm-none d-md-inline"> New Game</span>
                         </NavLink>
                     </li>
-                    <li className="nav-item">
-                        <NavLink exact to="/load-game" className="nav-link" title="Load Game">
-                            <i className="fas fa-save"></i>
-                            <span className="d-none d-sm-none d-md-inline"> Load Game</span>
-                        </NavLink>
-                    </li>
+                    {/* Conditionally render based on user prop*/}
+                    { user &&
+                        <li className="nav-item">
+                            <NavLink exact to="/load-game" className="nav-link" title="Load Game">
+                                <i className="fas fa-save"></i>
+                                <span className="d-none d-sm-none d-md-inline"> Load Game</span>
+                            </NavLink>
+                        </li>
+                    }
+
                 </ul>
 
                 <ul className="navbar-nav ml-md-auto">
-                    <li className="nav-item">
-                        <NavLink exact to="/create-account" className="nav-link" title="Create Account">
-                            <i className="fas fa-user-plus"></i>
-                            <span className="d-none d-sm-none d-md-inline"> Create Account</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink exact to="/log-in" className="nav-link" title="Login">
-                            <i className="fas fa-sign-in-alt"></i>
-                            <span className="d-none d-sm-none d-md-inline"> Login</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <NavLink exact to="/account" className="nav-link dropdown-toggle" title="User Name" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i className="fas fa-user-circle"></i>
-                            <span className="d-none d-sm-none d-md-inline"> User Name</span>
-                        </NavLink>
-                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <NavLink exact to="/account" className="dropdown-item" ><i className="fas fa-address-card"></i> Account</NavLink>
-                            <div className="dropdown-divider"></div>
-                            <NavLink exact to="/log-out" className="dropdown-item"><i className="fas fa-sign-out-alt"></i> Logout</NavLink>
-                        </div>
-                    </li>
+                    {/* Conditionally render based on user prop*/}
+                    { !user &&
+                    <Fragment>
+                            <li className="nav-item">
+                                <NavLink exact to="/create-account" className="nav-link" title="Create Account">
+                                    <i className="fas fa-user-plus"></i>
+                                    <span className="d-none d-sm-none d-md-inline"> Create Account</span>
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink exact to="/log-in" className="nav-link" title="Login">
+                                    <i className="fas fa-sign-in-alt"></i>
+                                    <span className="d-none d-sm-none d-md-inline"> Login</span>
+                                </NavLink>
+                            </li>
+                        </Fragment>
+                    }
+
+                    {/* Conditionally render based on user prop*/}
+                    { user &&
+                        <li className="nav-item dropdown">
+                            <NavLink exact to="/account" className="nav-link dropdown-toggle" title="User Name" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i className="fas fa-user-circle"></i>
+                                <span className="d-none d-sm-none d-md-inline"> User Name</span>
+                            </NavLink>
+                            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <NavLink exact to="/account" className="dropdown-item" ><i className="fas fa-address-card"></i> Account</NavLink>
+                                <div className="dropdown-divider"></div>
+                                <NavLink exact to="/log-out" className="dropdown-item" onClick={logOut}><i className="fas fa-sign-out-alt"></i> Logout</NavLink>
+                            </div>
+                        </li>
+                    }
+
                 </ul>
             </nav>
         </header>
