1c05108b3c5009492e6b89d2cfc497d46c7c1d60
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-07 23:35:59 -0700

    Creates "CreateAccount" components

--- rpg-frontend/src/App.js
+++ rpg-frontend/src/App.js
@@ -10,18 +10,25 @@ import CharacterSelect from './components/game_route/CharacterSelectPage.js';
 
 
 function App() {
+  // Backend URL
+  const backendURL = 'http://localhost:3001'
+  const usersURL = backendURL + '/users'
+  const charactersURL = backendURL + '/characters'
+  const abilitiesURL = backendURL + '/abilities'
+  const gameSavesURL = backendURL + '/game_saves'
+  const modifiersURL = backendURL + '/modifiers'
+
 
   return (
     <Router>
       <div>
         <Navbar />
-        {/* <Route exact path="/" component={Game}/> */}
-        {/* <Route exact path="/about" component={About} /> */}
-        
-        <Route exact path="/character-select" component={CharacterSelect} />
-        <Route exact path="/create-account" component={CreateAccount} />
-        <Route exact path="/Log-in" component={LogIn} />
+        <Route exact path="/new-game" component={Game}/>
         {/* <Route exact path="/load-game" component={load game ...} /> */}
+        {/* <Route exact path="/about" component={About} /> */}
+
+        <Route exact path="/create-account" component={() => <CreateAccount usersURL={usersURL}/>} />
+        <Route exact path="/Log-in" component={LogIn} />
       </div>
     </Router>
   );
--- rpg-frontend/src/components/Navbar.js
+++ rpg-frontend/src/components/Navbar.js
@@ -3,49 +3,49 @@ import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <header class="navbar navbar-expand navbar-dark bg-primary shadow flex-column flex-md-row bd-navbar">
-            <nav class="collapse navbar-collapse" >
+        <header className="navbar navbar-expand navbar-dark bg-primary shadow flex-column flex-md-row bd-navbar">
+            <nav className="collapse navbar-collapse" >
                 <NavLink exact to="/" className="navbar-brand" title="Flatiron Fight">
-                    <i class="fas fa-fist-raised"></i>
-                    <span class="d-none d-sm-none d-md-inline"> Flatiron Fight</span>
+                    <i className="fas fa-fist-raised"></i>
+                    <span className="d-none d-sm-none d-md-inline"> Flatiron Fight</span>
                 </NavLink>
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <NavLink exact to="/character-select" className="nav-link" title="New Game">
-                            <i class="fas fa-gamepad"></i>
-                            <span class="d-none d-sm-none d-md-inline"> New Game</span>
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <NavLink exact to="/new-game" className="nav-link" title="New Game">
+                            <i className="fas fa-gamepad"></i>
+                            <span className="d-none d-sm-none d-md-inline"> New Game</span>
                         </NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink exact to="/load-game" className="nav-link" title="Load Game">
-                            <i class="fas fa-save"></i>
-                            <span class="d-none d-sm-none d-md-inline"> Load Game</span>
+                            <i className="fas fa-save"></i>
+                            <span className="d-none d-sm-none d-md-inline"> Load Game</span>
                         </NavLink>
                     </li>
                 </ul>
 
-                <ul class="navbar-nav ml-md-auto">
-                    <li class="nav-item">
+                <ul className="navbar-nav ml-md-auto">
+                    <li className="nav-item">
                         <NavLink exact to="/create-account" className="nav-link" title="Create Account">
-                            <i class="fas fa-user-plus"></i>
-                            <span class="d-none d-sm-none d-md-inline"> Create Account</span>
+                            <i className="fas fa-user-plus"></i>
+                            <span className="d-none d-sm-none d-md-inline"> Create Account</span>
                         </NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink exact to="/log-in" className="nav-link" title="Login">
-                            <i class="fas fa-sign-in-alt"></i>
-                            <span class="d-none d-sm-none d-md-inline"> Login</span>
+                            <i className="fas fa-sign-in-alt"></i>
+                            <span className="d-none d-sm-none d-md-inline"> Login</span>
                         </NavLink>
                     </li>
-                    <li class="nav-item dropdown">
+                    <li className="nav-item dropdown">
                         <NavLink exact to="/account" className="nav-link dropdown-toggle" title="User Name" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-user-circle"></i>
-                            <span class="d-none d-sm-none d-md-inline"> User Name</span>
+                            <i className="fas fa-user-circle"></i>
+                            <span className="d-none d-sm-none d-md-inline"> User Name</span>
                         </NavLink>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <NavLink exact to="/account" className="dropdown-item" ><i class="fas fa-address-card"></i> Account</NavLink>
-                            <div class="dropdown-divider"></div>
-                            <NavLink exact to="/log-out" className="dropdown-item"><i class="fas fa-sign-out-alt"></i> Logout</NavLink>
+                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <NavLink exact to="/account" className="dropdown-item" ><i className="fas fa-address-card"></i> Account</NavLink>
+                            <div className="dropdown-divider"></div>
+                            <NavLink exact to="/log-out" className="dropdown-item"><i className="fas fa-sign-out-alt"></i> Logout</NavLink>
                         </div>
                     </li>
                 </ul>
