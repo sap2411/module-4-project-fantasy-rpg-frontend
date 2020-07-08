b073fb7b34232616b4ebc15a99fc88d0918a1c25
sap2411 <steven.parsons.us@gmail.com>
2020-07-07 18:04:56 -0700

    choose character to fight screen

--- rpg-frontend/src/components/Game.js
+++ rpg-frontend/src/components/Game.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import CharacterSelect from './game_route/CharacterSelectPage.js';
+import Fight from './game_route/Fight.js';
+const URL = 'http://localhost:3001/characters'
 
 
-function Game() {
+class Game extends Component{
+    state = {
+        collection: [],
+        character: null,
+        opponent: null
+    }
+
+    fetchCharacters = () => {
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(characters => {
+          let a = []
+          characters.data.forEach(data => a.unshift(data.attributes))
+          this.setState(prevState => {
+            return {collection: [...prevState.collection, ...a]}
+          })
+        })
+    }
+
+    componentDidMount = () => {
+        this.fetchCharacters()
+    }
+
+  chooseCharacter = (char) => {
+    let all = this.state.collection.filter(i => i != char)
+    let o = all[Math.floor(Math.random() * all.length)];
+    all = this.state.collection.filter(i => i != o)
+    this.setState({
+        collection: [...all],
+        character: char,
+        opponent: o
+    });
+    console.log(this.state)
+  }
     
-  return (
+  render() {
+    return (
     <div >
       <header ><br></br>
         <h1>Flatiron Fighter</h1>
-        <CharacterSelect />
+        {this.state.character ? <Fight player={this.state.character} opponent={this.state.opponent} /> : <CharacterSelect chooseCharacter={this.chooseCharacter} collection={this.state.collection} />}
       </header>
     </div>
-  );
+    );
+  }
 }
 
 
