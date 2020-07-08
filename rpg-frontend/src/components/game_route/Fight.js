70ca28d7f8353c820334125e4fdb6e569e24952d
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-08 12:01:57 -0700

    Add opponent attack to fight component

--- rpg-frontend/src/components/game_route/Fight.js
+++ rpg-frontend/src/components/game_route/Fight.js
@@ -12,19 +12,34 @@ class Fight extends Component {
       opponentBack: false,
       players_health: this.props.player.health,
       opponents_health: this.props.opponent.health,
-      log: ["Fight!"]
+      log: ["Fight!"],
+      aggressor: "player"
   }
 
   playerAttack = (abilityID) => {
+    // Pull selected ability
     const ability = this.props.player.abilities.find(ability => ability.id === parseInt(abilityID))
+    // update fight state
     this.setState(prevState => ({
       players_health: prevState.players_health + ability.healing_effect,
       opponents_health: prevState.opponents_health - ability.damage_effect,
-      log: [...this.state.log,`${this.props.player.name} used ${ability.name} to ${ability.descrription}`]
+      log: [`${this.props.player.name} used ${ability.name} to ${ability.description} (Damge: ${ability.damage_effect}, Heal: ${ability.healing_effect})`, ...prevState.log],
+      aggressor: "opponent"
     }))
+    // Call opponent to attack after a short timeout
+    setTimeout(this.opponentAttack, 2500)
   }
 
   opponentAttack = () => {
+    // Grab a random ability from the available 3
+    const ability = this.props.opponent.abilities[Math.floor(Math.random()*3)]
+    // Update fight state
+    this.setState(prevState => ({
+      players_health: prevState.players_health - ability.damage_effect,
+      opponents_health: prevState.opponents_health + ability.healing_effect,
+      log: [`${this.props.opponent.name} used ${ability.name} to ${ability.description} (Damge: ${ability.damage_effect}, Heal: ${ability.healing_effect})`, ...prevState.log],
+      aggressor: "player"
+    }))
   }
 
   onPlayerClick = (arg=false) => {
@@ -45,10 +60,9 @@ class Fight extends Component {
         {this.state.opponentBack ? <BackCard  character={this.props.opponent} handleDivClick={this.onOpponentClick} /> : <CharacterCard healthLeft={this.state.opponents_health}  character={this.props.opponent} handleDivClick={this.onOpponentClick} />}
       </div>
       <div className='ui two column centered grid'>
-        <AbilityButtons abilities={this.props.player.abilities} playerAttack={this.playerAttack}/>
+        <AbilityButtons abilities={this.props.player.abilities} playerAttack={this.playerAttack} aggressor={this.state.aggressor}/>
         <BattleLog log={this.state.log}/>
       </div>
-
     </div>
     );
   }
