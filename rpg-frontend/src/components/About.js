9d250201e10a3e6ca067daba9ea5b14cabf6f806
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-10 10:28:32 -0700

    rephrasing of about page

--- rpg-frontend/src/components/About.js
+++ rpg-frontend/src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div className="jumbotron rounded-lg col-6 py-5 mt-5 bg-white mx-auto text-center">
             <h1 className="display-4"><i className="fas fa-fist-raised"></i> Welcome to Flatiorn Fight <i className="fas fa-fist-raised"></i> </h1>
-            <p className="lead">The arena is the only place in the world where a Flatiorn student can prove they really have what it takes to make it as a software engineer! Battle your way through modules against your fellow classmates to see who can take on the instructors.</p>
+            <p className="lead">The arena is the only place where a Flatiorn student can prove they have what it really takes to make it as a software engineer. Battle against your fellow classmates to see who can take on the instructors!</p>
             <hr className="my-4"/>
             <p>Join the battle, prove yourself!</p>
             <Link exact to="/create-account" title="Create Account">
