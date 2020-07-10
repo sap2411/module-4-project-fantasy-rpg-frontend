4ad3afcdb3a615e28d6f8a14093a5c2607e5b722
sap2411 <steven.parsons.us@gmail.com>
2020-07-10 09:33:46 -0700

    victory gif added

--- rpg-frontend/src/components/game_route/GameOver.js
+++ rpg-frontend/src/components/game_route/GameOver.js
@@ -1,14 +1,25 @@
 import React from 'react';
-
 const GameOver = () => {
-
-
  return (
-     <div>
-         <h1>Game Over</h1>
-     </div>
-
+    <div class="gameover">
+    <h1>Game Over</h1> 
+    <br/>
+    <h3>Egg on your face</h3>
+  </div>
  )
 }
-
 export default GameOver
+// import React from 'react';
+
+// const GameOver = () => {
+
+
+//  return (
+//      <div>
+//          <h1>Game Over</h1>
+//      </div>
+
+//  )
+// }
+
+// export default GameOver
